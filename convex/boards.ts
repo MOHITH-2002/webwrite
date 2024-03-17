68acf31ec93a533e9c8c1b0c670154e7687fbdb3
MOHITH-2002 <mohithba9353@gmail.com>
2024-03-17 18:14:18 +0530

    removing caching

--- convex/boards.ts
+++ convex/boards.ts
@@ -1,6 +1,5 @@
 import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
-import { unstable_noStore as noStore } from "next/cache";
 export const get = query({ 
     
     args:{
@@ -8,7 +7,6 @@ export const get = query({
         search:v.optional(v.string())
     },
     handler: async (ctx,args)=>{
-        noStore();
         const identity = await ctx.auth.getUserIdentity();
         if(!identity)
         return;
